4d14b7a19fbd3081957ada38af7d02ff615bc53c
Siyam451 <Cseanowerhossensiyam@Gmail.com>
2025-09-01 09:32:03 -0700

    Live Assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'asssignment/home_Page.dart';
+import 'live_assignment/live_assignment.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: RecipeListScreen()
     );
   }
 }
